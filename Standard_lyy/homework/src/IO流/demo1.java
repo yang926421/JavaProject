d61769cf32a4a6d81fe875c6c3a164226c049372
小王子是你的 <1395348370@qq.com>
2020-07-08 23:44:37 +0800

    好吧 其实没写啥

--- Standard_lyy/homework/src/IO流/demo1.java
+++ Standard_lyy/homework/src/IO流/demo1.java
@@ -5,6 +5,6 @@ import java.io.File;
 public class demo1 {
     public static void main(String[] args) {
         File f1 = new File("d:...");
-        File f2 = new File("父路径", "zilujing ")
+        File f2 = new File("父路径", "zilujing ");
     }
 }
