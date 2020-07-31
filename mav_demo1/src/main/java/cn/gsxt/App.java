2801d04ac85423d38fc9f223c37daa5da6a13f26
yang92Yang <1395348370@qq.com>
2020-07-31 18:38:28 +0800

    maven

---
+++ mav_demo1/src/main/java/cn/gsxt/App.java
@@ -0,0 +1,11 @@
+package cn.gsxt;
+
+import org.junit.Test;
+
+public class App {
+
+    @Test
+    public void test(){
+        System.out.println(1234);
+    };
+}
---
+++ maven_web/maven_web2/src/main/java/cn/gsxt/servlet/MyServlet.java
@@ -0,0 +1,13 @@
+package cn.gsxt.servlet;
+
+import java.io.IOException;
+
+public class MyServlet extends sHttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
