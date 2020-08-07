6e5bc1c583fef631d65149c7f399f22e62849aa1
小王子是你的 <1395348370@qq.com>
2020-08-07 22:46:27 +0800

    家里电脑的提交

--- travel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
+++ travel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
@@ -10,26 +10,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-@WebServlet("/BaseServlet")
 public class BaseServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("重写的service方法");
-        String uri = req.getRequestURI();  //travel/user/add
-        System.out.println("请求全路径"+uri);
-        //对字符串切割获取方法名称
+        //1.获取请求路径
+        String uri = req.getRequestURI(); //   /travel/user/add
+        System.out.println("请求uri:"+uri);//  /travel/user/add
+        //2.获取方法名称
         String methodName = uri.substring(uri.lastIndexOf('/') + 1);
-        //获取方法对象method
-        System.out.println("方法名称"+methodName);
-        //根据反射获取方法对象
+        System.out.println("方法名称："+methodName);
+        //3.获取方法对象Method
+        //谁调用我？我代表谁
+        System.out.println(this);//UserServlet的对象cn.itcast.travel.web.servlet.UserServlet@4903d97e
         try {
             //获取方法
             Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
-            //执行方法
-            method.invoke(this, req, resp);
-        } catch (Exception e) {
+            //4.执行方法
+            //暴力反射
+            //method.setAccessible(true);
+            method.invoke(this,req,resp);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
     }
