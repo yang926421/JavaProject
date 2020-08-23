f447bfe4ea2b02306c7a197ab68c1cb822f64c74
小王子是你的 <1395348370@qq.com>
2020-08-23 22:19:23 +0800

    SSM整合

---
+++ SSM_WORKSPACE/ssm_lyy_project/src/main/java/cn/gsxt/mapper/AccountMapper.java
@@ -0,0 +1,13 @@
+package cn.gsxt.mapper;
+
+import cn.gsxt.domain.Account;
+
+import java.util.List;
+
+public interface AccountMapper {
+    //保存账户数据
+    public void save(Account account);
+    //查询账户数据
+    public List<Account> findAll();
+
+}
---
+++ SSM_WORKSPACE/ssm_lyy_project1/src/main/java/cn/gsxt/service/impl/AccountServiceImpl.java
@@ -0,0 +1,32 @@
+package cn.gsxt.service.impl;
+
+import cn.gsxt.domain.Account;
+import cn.gsxt.mapper.AccountMapper;
+import cn.gsxt.service.AccountService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public void save(Account account){
+        accountMapper.save(account);
+    }
+
+    @Override
+    public List<Account> findAll() {
+       return   accountMapper.findAll();
+    }
+}
