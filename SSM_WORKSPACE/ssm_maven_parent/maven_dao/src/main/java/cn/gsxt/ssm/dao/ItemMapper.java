92119b9f3e100aa4fa6fd8678625cfea6e861146
yang92Yang <1395348370@qq.com>
2020-08-24 18:09:05 +0800

    maven多模块协同开发

---
+++ SSM_WORKSPACE/ssm_maven_parent/maven_dao/src/main/java/cn/gsxt/ssm/dao/ItemMapper.java
@@ -0,0 +1,8 @@
+package cn.gsxt.ssm.dao;
+
+
+import cn.gsxt.ssm.pojo.Item;
+
+public interface ItemMapper {
+    public Item findById(int id);
+}
---
+++ SSM_WORKSPACE/ssm_maven_parent/maven_service/src/main/java/cn/gsxt/ssm/service/ItemService.java
@@ -0,0 +1,8 @@
+package cn.gsxt.ssm.service;
+
+
+import cn.gsxt.ssm.pojo.Item;
+
+public interface ItemService {
+    public Item findById(int id);
+}
